40676ee083906a7a33c738ed64da5b766807097a
Rukhsar Anwar <rukhsaranwar56@gmail.com>
2022-08-15 13:07:43 +0500

    add files

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+// function to dispaly the values
+function dis(val) {
+    document.getElementById("result").value += val
+}
+//function that evaluates the digit and return result
+function solve() {
+    let x = document.getElementById("result").value
+    let y = eval(x)
+    document.getElementById("result").value = y
+}
+function clr() {
+    document.getElementById("result").value = ""
+}
